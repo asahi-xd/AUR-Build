6fb123f2c5061b3bdd9eed7b8ac54f3dfa027f2a
Asahi <217256082+asahi-xd@users.noreply.github.com>
2026-07-10 09:57:01 +0530

    fixing

--- test.sh
+++ test.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-echo "Running the workflow..."
+WORKFLOW="aur-build.yml"
+REPO="asahi-xd/AUR-Build"
 
-#gh workflow run aur-build.yml -R asahi-xd/AUR-Build -f packages="$@" && sleep 5 && RUN_ID=$(gh run list --workflow=aur-build.yml -R asahi-xd/AUR-Build --limit 1 --json databaseId --jq '.[0].databaseId') && gh run -R asahi-xd/AUR-Build watch $RUN_ID && gh run -R asahi-xd/AUR-Build download $RUN_ID
-
+echo "Running the workflow...\n"
+echo "Building packages: $@"
 
 gh workflow run \
-    aur-build.yml -R asahi-xd/AUR-Build \
+    "$WORKFLOW" -R "$REPO" \
     -f packages="$@" \
-
     && sleep 5 \
-
-    && RUN_ID=$(gh run list --workflow=aur-build.yml -R asahi-xd/AUR-Build --limit 1 --json databaseId --jq '.[0].databaseId') \
-
-    && gh run -R asahi-xd/AUR-Build watch $RUN_ID \
-
-    && gh run -R asahi-xd/AUR-Build download $RUN_ID
-
-
-# Query the GitHub API for the artifact name
-#ARTIFACT_NAME=$(gh api repos/asahi-xd/AUR-Build/actions/runs/$RUN_ID/artifacts --jq '.artifacts[0].name')
-
-# Download using the dynamically fetched name
-#gh run download $RUN_ID -R asahi-xd/AUR-Build -n "$ARTIFACT_NAME" -D .
+    && RUN_ID=$(gh run list --workflow=$WORKFLOW -R $REPO --limit 1 --json databaseId --jq '.[0].databaseId') \
+    && gh run -R "$REPO" watch $RUN_ID \
+    && gh run -R "$REPO" download $RUN_ID \
+    && ARTIFACT_NAME=$(gh api repos/$REPO/actions/runs/$RUN_ID/artifacts --jq '.artifacts[0].name') \
+    && echo $ARTIFACT_NAME
 
